faa29e9ced1c51a0afc5ca468f9feee745d90503
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-05-27 11:27:06 +0000

    initial commit - copied from lerna plugin
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+    "env": {
+        "es2020": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:jest/recommended"
+    ],
+    "ignorePatterns": [
+        "**/*.js",
+        "**/*.d.ts"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "license-header",
+        "unused-imports"
+    ],
+    "rules": {
+        "max-len": ["warn", 150],
+        "no-console": "error",
+        "no-multiple-empty-lines": "warn",
+        "no-trailing-spaces": "warn",
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/comma-dangle": ["warn", "only-multiline"],
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/indent": ["warn", 4],
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-magic-numbers": ["warn", {
+            "ignore": [-1, 0, 1, 2],
+            "ignoreDefaultValues": true,
+            "ignoreReadonlyClassProperties": true
+        }],
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            "argsIgnorePattern": "^_"
+        }],
+        "license-header/header": ["error", "./LICENSE_HEADER"],
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/semi": "warn",
+        "unused-imports/no-unused-imports": "warn",
+        "unused-imports/no-unused-vars": ["warn", {
+            "args": "none"
+        }]
+    }
+};
---
+++ src/init.ts
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2022 Octorelease Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as fs from "fs";
+import { IContext } from "@octorelease/core";
+import { DEFAULT_NPM_REGISTRY, utils as npmUtils } from "@octorelease/npm";
+import { IPluginConfig } from "./config";
+
+export default async function (context: IContext, _config: IPluginConfig): Promise<void> {
+    if (context.env.NPM_TOKEN == null) {
+        throw new Error("Required environment variable NPM_TOKEN is undefined");
+    }
+
+    let publishConfig;
+    try {
+        const lernaJson = JSON.parse(fs.readFileSync("lerna.json", "utf-8"));
+        context.version.new = lernaJson.version;
+        publishConfig = lernaJson.publish;
+    } catch {
+        context.logger.warning(`Missing or invalid lerna.json in branch ${context.branch.name}`);
+    }
+
+    try {
+        context.workspaces = JSON.parse(fs.readFileSync("package.json", "utf-8")).workspaces;
+    } catch {
+        context.logger.warning(`Missing or invalid package.json in branch ${context.branch.name}`);
+    }
+
+    context.branch.channel = context.branch.channel || "latest";
+    await npmUtils.npmConfig(context, publishConfig?.registry || DEFAULT_NPM_REGISTRY);
+}
